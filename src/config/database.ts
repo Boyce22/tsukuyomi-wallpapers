bf780c5954a2983aa58a261df39982fe095b48d8
Bernardo Alves <bernardojob2003@outlook.com>
2025-06-14 16:40:32 -0300

    TSU-1
    feat(DB): adição dos comentários em cada coluna

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,6 @@
 import 'dotenv/config';
 
 import { DataSource } from 'typeorm';
-import { Tag } from '@/models/tag';
-import { User } from '@/models/user';
-import { Role } from '@/models/role';
-import { Wallpaper } from '@/models/wallpaper';
 
 const PORT = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432;
 
@@ -17,7 +13,7 @@ const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [Wallpaper, Tag, User, Role],
+  entities: ['src/models/*.ts'],
   migrationsTableName: 'migrations',
 });
 
