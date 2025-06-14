16e6aa9ba19cb275f5ed217dca05cd760b0fc22e
Bernardo Alves <bernardojob2003@outlook.com>
2025-06-14 18:32:24 -0300

    TSU-1
    feat(Routes): manipulação/criação das rotas da aplicação

--- src/routes/_index.ts
+++ src/routes/_index.ts
@@ -1,8 +1,20 @@
-import express from "express";
-const router = express.Router();
+import { Router } from 'express';
 
-import wallpapersRoutes from "./wallpapers";
+import TagService from '@/services/tags';
+import WallpaperService from '@/services/wallpapers';
 
-router.use("/wallpapers", wallpapersRoutes);
+import { TagRepository } from '@/repositories/tags';
+import WallpaperRepository from '@/repositories/wallpapers';
+
+import { createTagsRouter } from './tags';
+import { createWallpapersRouter } from '@/routes/wallpapers';
+
+const router = Router();
+
+const tagService = TagService.createInstance(TagRepository.createInstance());
+const wallpaperService = WallpaperService.createInstance(WallpaperRepository.createInstance());
+
+router.use('/tags', createTagsRouter(tagService));
+router.use('/wallpapers', createWallpapersRouter(wallpaperService, tagService));
 
 export default router;
