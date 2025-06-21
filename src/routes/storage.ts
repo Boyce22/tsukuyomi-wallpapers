bbd984d30e549492263bd4fe3a8966c480fe4e60
Bernardo Alves <bernardojob2003@outlook.com>
2025-06-21 14:32:59 -0300

    TSU-005
    feat(Storage): requisição responsável por obter os buckets

---
+++ src/routes/storage.ts
@@ -0,0 +1,13 @@
+import { IStorageService } from '@/_types/storage/storage.type';
+import StorageController from '@/controllers/storage';
+import { Router, type Request, type Response } from 'express';
+
+
+export const createStorageRouter = (storageService: IStorageService) => {
+    const router = Router()
+    const controller = StorageController.createInstance(storageService);
+
+    router.get('/buckets', (req: Request, res: Response) => controller.getBuckets(req, res))
+
+    return router
+}
