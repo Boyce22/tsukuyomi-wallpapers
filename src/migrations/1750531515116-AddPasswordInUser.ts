af5b71fcc401612e11ae9b47a70c39b3de50a9ee
Bernardo Alves <bernardojob2003@outlook.com>
2025-06-21 15:45:31 -0300

    TSU-5
    feat(User): adição da senha no modelo ORM

---
+++ src/migrations/1750531515116-AddPasswordInUser.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddPasswordInUser1750531515116 implements MigrationInterface {
+    name = 'AddPasswordInUser1750531515116'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" ADD "password" character varying(100) NOT NULL`);
+        await queryRunner.query(`COMMENT ON COLUMN "user"."password" IS 'Hash da senha do usuário (máx. 100 caracteres).'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`COMMENT ON COLUMN "user"."password" IS 'Hash da senha do usuário (máx. 100 caracteres).'`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "password"`);
+    }
+
+}
